48c4b10a61568c48482eb521897698d66db67a7e
olivier-schmitt <olivierursushorribilis@gmail.com>
2017-11-11 08:52:13 +0100

    use node:6.11 image instead john papa's ngcli 1.5.0 add node remote port for debug

--- ngcli.sh
+++ ngcli.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run --rm -v ${PWD}:/root/ngcli --name ngcli quay.io/olivier_schmitt/ng4dev-cli:latest ng $*
+docker run --rm -v ${PWD}:/root/ngcli quay.io/olivier_schmitt/ng4dev-cli:latest ng $*
