66843d3d55daf16e2ffe303acf994fbddee44b2b
olivier-schmitt <olivierursushorribilis@gmail.com>
2017-11-11 09:28:37 +0100

    Migration to 1.5.0

--- serve.sh
+++ serve.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run --rm -v ${PWD}:/root/ngcli -p 5858:5858 -p 4200:4200 --name ngcli quay.io/olivier_schmitt/ng4dev-cli:latest ng serve --host 0.0.0.0 $*
+docker run --rm -v ${PWD}:/root/ngcli -p 5858:5858 -p 4200:4200 --name ngcli-1.5.0 quay.io/olivier_schmitt/ng4dev-cli:1.5.0 ng serve --host 0.0.0.0 $*
